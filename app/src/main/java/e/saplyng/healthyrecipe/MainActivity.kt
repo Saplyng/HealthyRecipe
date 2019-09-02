9fa25672df631a9e02f8dc8d2eaffffd03df6343
Jeremy Stein <saplyng@gmail.com>
2019-09-02 14:58:04 -0500

    Initial commit

---
+++ app/src/main/java/e/saplyng/healthyrecipe/MainActivity.kt
@@ -0,0 +1,22 @@
+package e.saplyng.healthyrecipe
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        idButton.setOnClickListener{
+            val intent = Intent(this, recipe::class.java)
+
+            startActivity(intent)
+
+
+        }
+    }
+}
---
+++ app/src/main/java/e/saplyng/healthyrecipe/recipe.kt
@@ -0,0 +1,12 @@
+package e.saplyng.healthyrecipe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class recipe : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_recipe)
+    }
+}
